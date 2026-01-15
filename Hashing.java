f2bca5e050df2f0cb20e3714fbb14be5bdaec5c6
Ajay Negi <ajnegi456@gmail.com>
2026-01-15 15:28:38 +0000

    read me

---
+++ Hashing.java
@@ -0,0 +1,16 @@
+//Contains duplicates
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0; i <nums.length;i++){
+            if(hs.contains(nums[i])){
+                return true;
+            }
+            else{
+                hs.add(nums[i]);
+        
+            }
+        }
+        return false;
+    }
+}
