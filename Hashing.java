f35792fb81ba4f7ea815f759b78484c6f799d92c
Ajay Negi <ajnegi456@gmail.com>
2026-01-16 07:47:41 +0530

    Add longestConsecutive method to Solution class
    Implement longest consecutive sequence method using HashSet.

--- Hashing.java
+++ Hashing.java
@@ -32,3 +32,29 @@ class Solution {
 
     }
 }
+
+//longest consecutive sequence                        //medium
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            hs.add(nums[i]);
+        }
+        int maxlen =0;
+        for(int num : hs){
+            if(!hs.contains(num -1)){
+                int currNum = num;
+                int currLen = 1;
+
+                while(hs.contains(currNum + 1)){
+                    currNum++;
+                    currLen ++;
+                }
+               if (currLen > maxlen) {
+                      maxlen = currLen;
+                    }
+            }
+        }
+        return maxlen;
+    }
+}
