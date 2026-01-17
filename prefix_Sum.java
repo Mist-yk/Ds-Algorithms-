fe5be0a3481ec6722933536afdc02ced06a62945
Ajay Negi <ajnegi456@gmail.com>
2026-01-17 20:56:27 +0530

    Update prefix_Sum.java

--- prefix_Sum.java
+++ prefix_Sum.java
@@ -1,1 +1,19 @@
-
+//Range Sum Query - Immutable                                 //easy
+class NumArray {
+    private int[] prefix_arr;
+    public NumArray(int[] nums) {
+        prefix_arr = new int[nums.length + 1];
+        prefix_arr[0] = 0;
+        for(int i=0; i < nums.length; i++){
+            prefix_arr[i + 1] = prefix_arr[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        // if(left == 0){
+        //     return prefix_arr[right];                  //edge case 
+        // }
+        return prefix_arr[right  + 1] - prefix_arr[left];
+        
+    }
+}
