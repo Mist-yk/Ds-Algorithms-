242d48c85461bee6ac83e98d7bee56e643aadf38
Ajay Negi <ajnegi456@gmail.com>
2026-01-17 21:46:46 +0530

    Add subarray sum method to Solution class
    Implemented a method to count subarrays that sum to k using a HashMap.

--- prefix_Sum.java
+++ prefix_Sum.java
@@ -17,3 +17,20 @@ class NumArray {
         
     }
 }
+
+//Subarray Sum Equals K                                      //medium
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count =0,sum =0;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        hm.put(0,1);
+        for(int i=0; i < nums.length; i++){
+            sum +=nums[i];    //stores the sum
+            if(hm.containsKey(sum - k)){
+                count += hm.get(sum - k);
+            }
+            hm.put(sum ,hm.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
