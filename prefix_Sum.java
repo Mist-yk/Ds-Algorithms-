e48ab985f78424b33e42e27d8774b256d97a667e
Ajay Negi <ajnegi456@gmail.com>
2026-01-20 23:01:56 +0530

    Add waysToSplitArray method to count splits
    Implement method to count ways to split array based on sums.

--- prefix_Sum.java
+++ prefix_Sum.java
@@ -99,3 +99,29 @@ class Solution {
         return result;
     }
 }
+
+
+
+//Number of ways to split array                                      //medium
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+
+        int []arr = new int[nums.length + 1];
+        long rightsum = 0,leftsum=0;
+        for(int num : nums){
+            rightsum += num;
+        }
+
+        int count =0;
+        for(int i =0 ; i <nums.length -1; i++){
+            leftsum += nums[i];
+            rightsum -= nums[i];
+
+            if(leftsum >= rightsum){
+                count++;
+            }
+        }
+
+            return count;
+    }
+}
