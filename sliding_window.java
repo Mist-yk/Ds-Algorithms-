89e8d54a61753db97eafb2954fe64cbe88c8f570
Ajay Negi <ajnegi456@gmail.com>
2026-01-25 22:56:14 +0530

    Update sliding_window.java

--- sliding_window.java
+++ sliding_window.java
@@ -1,3 +1,4 @@
+//Fixed window size
 //Maximum subarray average
 
 class Solution {
@@ -16,3 +17,49 @@ class Solution {
         return (double)max_sum/k;
     }
 }
+
+//Maximum Sum of Distinct Subarrays With Length K
+
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+       Set<Integer> s = new HashSet<>();
+       long max=0, sum =0;
+       int left = 0;
+       for(int i=0; i<nums.length; i++){
+        while(s.contains(nums[i]) || s.size() == k){
+            s.remove(nums[left]);
+            sum -= nums[left++];
+        }
+        sum += nums[i];
+        s.add(nums[i]);
+        if(s.size() == k){
+            max = Math.max(max,sum);
+        }
+       }
+       return max;
+    }
+} 
+
+
+
+//variable size
+//Longest Substring Without Repeating Characters
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        HashSet<Character> hs = new HashSet<>();
+        int maxlen = 0;
+        int left =0;
+        
+        for(int right =0; right < n; right++){
+            while(hs.contains(s.charAt(right))){
+                hs.remove(s.charAt(left));
+                left++;
+            }
+            hs.add(s.charAt(right));
+            maxlen = Math.max(maxlen , right - left + 1);
+        }
+        return maxlen;
+    }
+}
+                      
