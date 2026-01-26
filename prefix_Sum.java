5390dc27f3923d5f562bd38aa0785078e37667f9
Ajay Negi <ajnegi456@gmail.com>
2026-01-26 14:02:23 +0530

    Update prefix_Sum.java

--- prefix_Sum.java
+++ prefix_Sum.java
@@ -149,24 +149,3 @@ class Solution {
 }
 
 
-//Maximum Sum of Distinct Subarrays With Length K
-
-class Solution {
-    public long maximumSubarraySum(int[] nums, int k) {
-       Set<Integer> s = new HashSet<>();
-       long max=0, sum =0;
-       int left = 0;
-       for(int i=0; i<nums.length; i++){
-        while(s.contains(nums[i]) || s.size() == k){
-            s.remove(nums[left]);
-            sum -= nums[left++];
-        }
-        sum += nums[i];
-        s.add(nums[i]);
-        if(s.size() == k){
-            max = Math.max(max,sum);
-        }
-       }
-       return max;
-    }
-}
