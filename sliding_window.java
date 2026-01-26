36a2e0795e4e8a182ea0f161d9b5f8c2a416cb24
Ajay Negi <ajnegi456@gmail.com>
2026-01-26 14:20:50 +0530

    Add method to count subarrays with average threshold

--- sliding_window.java
+++ sliding_window.java
@@ -40,6 +40,33 @@ class Solution {
     }
 }
 
+//Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int sum =0;
+        int count =0 ;
+        int left =0 ;
+         // int avg = sum /k;
+        int target = threshold * k;
+        for(int i =0; i < k ; i++){
+            sum += arr[i];
+        }
+        if(sum >= target){
+            count ++;
+        }
+        for(int r = k; r < arr.length; r++){
+            sum += arr[r];
+            sum -= arr[r - k];
+
+            if(sum >= target){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
 
 
 
