a496aa4341fc531eba6733d034447509c2195652
Ajay Negi <ajnegi456@gmail.com>
2026-02-19 08:11:40 +0530

    Update Hashing.java

--- Hashing.java
+++ Hashing.java
@@ -82,3 +82,27 @@ class Solution {
         return result;
     }
 }
+
+
+
+//Two sum                                                                                     easy
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hs= new HashMap<>();
+        int n =nums.length;
+        // for(int i =0;i < n; i++){
+        //     hs.put(nums[i],i);
+        // }
+        for(int i =0; i <n; i++){
+            int complement = target - nums[i];
+            if(hs.containsKey(complement)){
+                return new int []{hs.get(complement),i}; //hs.get(complement) → gives index of previously seen number i →                                                                                    current index
+                }
+                hs.put(nums[i],i);
+        }
+        return new int[]{};
+
+
+
+    }
+}
