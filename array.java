3de736e96b60c30fd3a831ca8582dee21628e254
Ajay Negi <ajnegi456@gmail.com>
2026-02-20 09:17:42 +0530

    Add array rotation function
    Implemented array rotation by reversing segments.

---
+++ array.java
@@ -0,0 +1,24 @@
+//to rotate any arry with k steps the intution is to reverse from begin to k and then k to n -1; then 0 to n-1;
+
+
+//Rotate arry by k
+class Solution {
+    static void rotateArr(int arr[], int d) {
+        int n = arr.length;
+        d = d % n;   
+        
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+    
+    private static void reverse(int arr[], int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
